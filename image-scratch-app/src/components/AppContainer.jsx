87a35e29373a1595131fe4fe8f48b5b10a7194fa
MaryHooker <mc020791@yahoo.com>
2020-04-15 08:55:37 -0500

    checked

---
+++ image-scratch-app/src/components/AppContainer.jsx
@@ -0,0 +1,17 @@
+import React,{Component} from 'react';
+
+class AppContainer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <div>
+               <img src="/bettyBoop.png" alt=""/>
+            </div>
+         );
+    }
+}
+ 
+export default AppContainer;
---
+++ script.js
@@ -0,0 +1,13 @@
+// console.log("Hello World");
+
+// let homeTeamScore="72";
+// let visitorTeamScore="86";
+
+// let finalTeamScore=
+// `
+// The Final Score was:
+// Home Team - ${homeTeamScore}
+// Visitor Team - ${visitorTeamScore}
+
+// `
+// console.log(finalTeamScore)
